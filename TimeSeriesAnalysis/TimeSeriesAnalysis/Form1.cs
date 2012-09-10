dbdcbf7bd85b831518ab20c8f2fcd6eddf8f263a
Alexander Gorbunov <sanyo.gorbunov@gmail.com>
2012-09-10 11:25:25 +0300

    Added LINQToCSV library.

--- TimeSeriesAnalysis/TimeSeriesAnalysis/Form1.cs
+++ TimeSeriesAnalysis/TimeSeriesAnalysis/Form1.cs
@@ -9,11 +9,28 @@ using System.Windows.Forms;
 
 namespace TimeSeriesAnalysis
 {
+    using LINQtoCSV;
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //CsvFileDescription desc = new CsvFileDescription
+                //{
+                //    SeparatorChar = ','
+                //};
+                CsvContext context = new CsvContext();
+                IEnumerable<DataRow> rows = context.Read<DataRow>(dialog.FileName);
+                foreach (var row in rows)
+                {
+                    
+                }
+            }
         }
     }
 }
